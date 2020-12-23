55e13928c38c81ddf78bab93c4cb84ca3510a5e3
samuelmale <samuelsmalek@gmail.com>
2020-12-23 12:25:21 +0300

    Add attachments widgets

---
+++ src/attachments/image-preview.component.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import styles from './image-preview.css';
+import { useTranslation } from 'react-i18next';
+import { UserHasAccessReact } from '@openmrs/esm-api';
+
+export default function ImagePreview(props: ImagePreviewProps) {
+  const [caption, setCaption] = useState('');
+  const { t } = useTranslation();
+
+  function saveImage(e: React.SyntheticEvent) {
+    e.preventDefault();
+    props.onSaveImage(props.dataUri, caption);
+  }
+
+  function cancelCapture(e: React.SyntheticEvent) {
+    e.preventDefault();
+    props.onCancelCapture();
+  }
+
+  function handleSubmit(e: React.SyntheticEvent) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  function updateCaption(e: React.ChangeEvent<HTMLInputElement>) {
+    e.preventDefault();
+    e.stopPropagation();
+    setCaption(e.target.value);
+  }
+
+  return (
+    <form className={styles.overview} onSubmit={handleSubmit}>
+      <img src={props.dataUri} alt={t('webcamPreview', 'Webcam preview')} />
+      <input
+        type="text"
+        placeholder={t('attachmentCaptionInstruction', 'Enter a caption for the image')}
+        onChange={updateCaption}
+      />
+      <UserHasAccessReact privilege="Create Attachment">
+        <button onClick={saveImage}>{t('save', 'Save')} </button>
+      </UserHasAccessReact>
+      <button onClick={cancelCapture}>{t('cancel', 'Cancel')} </button>
+    </form>
+  );
+}
+
+type ImagePreviewProps = {
+  dataUri: string;
+  onSaveImage?: Function;
+  onCancelCapture?: Function;
+};
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export { default as AttachmentThumbnail } from './attachments/attachment-thumbnail.component';
+export { default as AttachmentsOverview } from './attachments/attachments-overview.component';
